d3cedf25ec4b4a04d328c96ce8a5fb2ac614974b
MattCress99 <54798864+MatthewCresswell@users.noreply.github.com>
2023-06-23 14:46:36 +0200

    added cube colour change with Arrow keys

--- Zutari Interview/Assets/Scripts/CubeController.cs
+++ Zutari Interview/Assets/Scripts/CubeController.cs
@@ -24,28 +24,28 @@ public class CubeController : MonoBehaviour
     private void ChangeCubeColour()
     {
         
-        if(Input.GetKey(KeyCode.W)) //if W is pressed change colour to Red
+        if(Input.GetKey(KeyCode.W) || Input.GetKey(KeyCode.UpArrow)) //if W is pressed change colour to Red
         {
             cubeColor = new Color(255f, 0f, 0f, 0f);
             myRenderer.material.color = cubeColor;
             cubeMat = myRenderer.material;
             trail.material = cubeMat;
         }
-        if(Input.GetKey(KeyCode.A)) //if A is pressed change colour to Blue
+        if(Input.GetKey(KeyCode.A) || Input.GetKey(KeyCode.LeftArrow)) //if A is pressed change colour to Blue
         {
             cubeColor = new Color(0f, 81f, 255f, 0f);
             myRenderer.material.color = cubeColor;
             cubeMat = myRenderer.material;
             trail.material = cubeMat;
         }
-        if(Input.GetKey(KeyCode.S)) //if S is pressed change colour to Yellow
+        if(Input.GetKey(KeyCode.S) || Input.GetKey(KeyCode.DownArrow)) //if S is pressed change colour to Yellow
         {
             cubeColor = new Color(191f, 80f, 0f, 0f);
             myRenderer.material.color = cubeColor;
             cubeMat = myRenderer.material;
             trail.material = cubeMat;
         }
-        if(Input.GetKey(KeyCode.D)) //if D is pressed change colour to Pink
+        if(Input.GetKey(KeyCode.D) || Input.GetKey(KeyCode.RightArrow)) //if D is pressed change colour to Pink
         {
             cubeColor = new Color(192f, 0f, 255f, 0f);
             myRenderer.material.color = cubeColor;
@@ -54,7 +54,7 @@ public class CubeController : MonoBehaviour
         }
     }
 
-    public void ChangeSpeed()
+    public void ChangeSpeed() // adjust speed with J & K keys
     {
         if(Input.GetKey(KeyCode.J))
         {
@@ -68,27 +68,28 @@ public class CubeController : MonoBehaviour
         moveSpeedText.text = "Speed: " + moveSpeed.ToString() + "\n  -J | K+";
     }
 
-    public void IncreaseSpeed()
+    public void IncreaseSpeed() //increase speed button
     {
         speed *= 1.25f;
     }
-    public void DecreaseSpeed()
+    public void DecreaseSpeed() //decrease speed button
     {
         speed *= 0.9f;
     }
 
     void Update()
     {
+        //allows cube to move along x & y access
         Vector2 input = new Vector2(Input.GetAxis("Horizontal"), Input.GetAxis("Vertical"));
         input.Normalize();
         Vector3 velocity = speed * input;
         Vector3 tempPosition = transform.localPosition + velocity * Time.deltaTime;
-        if(screenBound.IsOutOfBounds(tempPosition))
+        if(screenBound.IsOutOfBounds(tempPosition))//check if the cube is out of screen bounds, if true recalculates position
         {
             Vector2 newPosition = screenBound.CalculateWrappedPosition(tempPosition);
             transform.position = newPosition;
         }
-        else
+        else//if false keep cube in current position
         {
             transform.position = tempPosition;
         }
